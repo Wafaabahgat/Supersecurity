c5145e78dbda1f5d6de82085a2fb9fb1622ade87
Wafaa <Wafaabahgat>
2023-12-02 03:21:21 +0300

    v

--- src/components/services/Steps.tsx
+++ src/components/services/Steps.tsx
@@ -10,8 +10,8 @@ const Steps: FC<StepsProps> = () => {
   return (
     <>
       <div className="mt-20">
-        <Container className="relative py-10">
-          <div className="absolute top-0 left-0 w-full h-full z-0">
+        <Container className="relative xl:py-10 py-4">
+          <div className="absolute top-0 left-0 w-full h-full z-0 md:block hidden">
             <img src={BgSteps} alt="steps" className="w-full h-full" />
           </div>
           {/* h-[639px] */}
@@ -19,18 +19,18 @@ const Steps: FC<StepsProps> = () => {
             <h2 className="lg:text-[36px] md:text-[30px] sm:text-[25px] text-[18px]">
               Easy steps to start your pentest
             </h2>
-            <div className="grid grid-cols-3 px-10 mt-7 gap-6">
-              <div className="flex flex-col items-center justify-center gap-28">
-               <StepsGroup/>
-               <StepsGroup/>
-               
+            <div className="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 xl:px-10 px-16 mt-7 xl:gap-6">
+              <div className="flex flex-col items-center justify-center xl:gap-24 md:gap-12 ">
+                <StepsGroup num="01" ttl="pre-test" className=" mb-6"/>
+                <StepsGroup num="02" ttl="test" className=" mb-6"/>
               </div>
-              <div className="w-full h-full flex items-center justify-center">
+              <div className="hidden w-full h-full xl:flex items-center justify-center">
                 <img src={illustration} className=" max-w-[502px]" />
               </div>
-              <div className="flex flex-col items-center justify-center gap-28">
-              <StepsGroup/>
-               <StepsGroup/>
+
+              <div className="flex flex-col items-center justify-center xl:gap-24 md:gap-12">
+                <StepsGroup num="03" ttl="preporting" className=" mb-6"/>
+                <StepsGroup num="04" ttl="review" className=" mb-6"/>
               </div>
             </div>
           </div>
--- src/components/services/StepsGroup.tsx
+++ src/components/services/StepsGroup.tsx
@@ -1,23 +1,26 @@
+import { cn } from "@/utils";
 import { FC } from "react";
 
 interface StepsGroupProps {}
 
-const StepsGroup: FC<StepsGroupProps> = () => {
+const StepsGroup: FC<StepsGroupProps> = ({ num, ttl, className }) => {
   return (
     <>
-      <div>
-        <div className="flex gap-4 items-center justify-center">
+      <div className={cn(className, "")}>
+        <div className="flex items-center justify-center relative">
           {/*  */}
-          <div className="flex flex-col shadow-3xl border-b border-slate-200 border rounded-full px-3 ">
-            <h6 className=" uppercase text-[13px]">step</h6>
-            <span className="font-medium text-[21px]">01</span>
+          <div className="flex flex-col absolute -top-2 left-6 bg-white shadow-md transition-transform rounded-full px-3 ">
+            <h6 className="uppercase text-[13px]">step</h6>
+            <span className="font-medium text-[21px]">{num}</span>
           </div>
-
-          <h4 className="font-semibold">PRE-TEST</h4>
+          {/* ml-28 */}
+          <h4 className="font-semibold text-[18px]  uppercase mx-auto">
+            {ttl}
+          </h4>
         </div>
-        <p className="text-[12px] bg-main-light rounded-md p-6 max-w-[291px] text-start text-white">
-          Lorem ipsum dolor sit amet consectetur. Fusce enim etiam vitae eu
-          velit amet diam nunc. Lorem elit non dignissim molestie ut.
+        <p className="text-[12px] bg-main-light rounded-3xl border-l-4 border-main  max-w-[291px] py-7 px-5 text-start text-white">
+          Lorem ipsum dolor sit amet consectetur. Fusce enim etiam vitae e velit
+          amet diam nunc. Lorem elit non dignissim molestie ut.
         </p>
       </div>
     </>
